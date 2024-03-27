72525ea4774308cd18b043eda50ed776c3b43370
APISXD <159783293+APISXD@users.noreply.github.com>
2024-03-28 00:40:32 +0700

    Add files via upload

---
+++ js/words.js
@@ -0,0 +1,226 @@
+let wordList = [
+    {
+        word: "amanda",
+        hint: "🏍️"
+    },
+    {
+        word: "christy",
+        hint: "🐟"
+    },
+    {
+        word: "lia",
+        hint: "⚽"
+    },
+    {
+        word: "zee",
+        hint: "😎"
+    },
+    {
+        word: "callie",
+        hint: "😢😭😿"
+    },
+    {
+        word: "oniel",
+        hint: "bapak bapak"
+    },
+    {
+        word: "olla",
+        hint: "🏎️ horas"
+    },
+    {
+        word: "feni",
+        hint: "idol bingits"
+    },
+    {
+        word: "fiony",
+        hint: "🐣"
+    },
+    {
+        word: "flora",
+        hint: "bogel"
+    },
+    {
+        word: "freya",
+        hint: "😄🍫"
+    },
+    {
+        word: "ella",
+        hint: "hissatsu"
+    },
+    {
+        word: "gita",
+        hint: "🧊🌨️❄️⛄"
+    },
+    {
+        word: "gracie",
+        hint: "xixixi"
+    },
+    {
+        word: "greesel",
+        hint: "lutunaa"
+    },
+    {
+        word: "eli",
+        hint: "kagetnya luar biasa"
+    },
+    {
+        word: "indah",
+        hint: "mommy"
+    },
+    {
+        word: "indira",
+        hint: "teteh bandung"
+    },
+    {
+        word: "jessi",
+        hint: "temen olla"
+    },
+    {
+        word: "lyn",
+        hint: "cici pik"
+    },
+    {
+        word: "kathrina",
+        hint: "bokem"
+    },
+    {
+        word: "lulu",
+        hint: "ckp"
+    },
+    {
+        word: "marsha",
+        hint: "anime"
+    },
+    {
+        word: "muthe",
+        hint: "dont touch!"
+    },
+    {
+        word: "raisha",
+        hint: "adek bongsor"
+    },
+    {
+        word: "adel",
+        hint: "abang abang katanya"
+    },
+    {
+        word: "shani",
+        hint: "lebih lebih dari"
+    },
+    {
+        word: "gracia",
+        hint: "sekali lagii"
+    },
+    {
+        word: "aralie",
+        hint: "Seperti bunga yang mekar"
+    },
+    {
+        word: "delynn",
+        hint: "pyong pyong"
+    },
+    {
+        word: "shasa",
+        hint: "horas we"
+    },
+    {
+        word: "alya",
+        hint: "poyang"
+    },
+    {
+        word: "anin",
+        hint: "🥭"
+    },
+    {
+        word: "lana",
+        hint: "Dengan Kekuatan bulan"
+    },
+    {
+        word: "erine",
+        hint: "checkmate!"
+    },
+    {
+        word: "cathy",
+        hint: "ampun biyung, apa kau"
+    },
+    {
+        word: "elin",
+        hint: "Semanis permen kapas"
+    },
+    {
+        word: "chelsea",
+        hint: "bule jamet"
+    },
+    {
+        word: "cynthia",
+        hint: "pop pop"
+    },
+    {
+        word: "danella",
+        hint: "toktok 👋"
+    },
+    {
+        word: "daisy",
+        hint: "seperti bunga daisy"
+    },
+    {
+        word: "fritzy",
+        hint: "pesulap"
+    },
+    {
+        word: "gendis",
+        hint: "the next diva 🎤"
+    },
+    {
+        word: "lily",
+        hint: "piw piw jumpshot"
+    },
+    {
+        word: "trisha",
+        hint: "one two thriii"
+    },
+    {
+        word: "jeane",
+        hint: "victoriaa!"
+    },
+    {
+        word: "moreen",
+        hint: "One and only"
+    },
+    {
+        word: "michie",
+        hint: "hello everyone it's"
+    },
+    {
+        word: "levi",
+        hint: "kapten aot"
+    },
+    {
+        word: "nayla",
+        hint: "baru burajar"
+    },
+    {
+        word: "nachia",
+        hint: "panda imut katanya"
+    },
+    {
+        word: "oline",
+        hint: "oniel versi kalem"
+    },
+    {
+        word: "regie",
+        hint: "basket 🏀"
+    },
+    {
+        word: "ribka",
+        hint: "lord ribi"
+    },
+    {
+        word: "nala",
+        hint: "ROAR!"
+    },
+    {
+        word: "kimmy",
+        hint: "baby hamster"
+    },
+]
